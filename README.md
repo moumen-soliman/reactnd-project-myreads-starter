857cd3ef543bfc7c075aa0faf020a5f066e1849c
moumen-soliman <moumensoliman@gmail.com>
2020-04-03 14:30:19 +0200

    Fix search issue and update page

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# Project Overview
+# MyReads Project
 
-In the MyReads project, you'll create a bookshelf app that allows you to select and categorize books you have read, are currently reading, or want to read. The project emphasizes using React to build the application and provides an API server and client library that you will use to persist information as you interact with the application.
+This game was built for the Udacity React Nanodegree Program. The purpose of the project is to demonstrate understanding of the basic structure and operation of a React-based app.
 
-Either fork and clone the starter template or use create-react-app to create a new application. Even if you create your own application from scratch you'll still need to use the booksAPI.js file that comes with the starter template to interact with the backend API. Instructions for using the API methods are provided in the README in the starter template repository.
+# How to Load the App
 
-The starter template contains all the CSS and HTML markup that may be used but omits the React code that is required to complete the project. This can save you some time if you don't wish to write all the CSS and HTML from scratch. The provided code will demonstrate a static HTML page of the finished application, but with no interactive functionality.
+You can run a hosted version of the app at https://sharp-poitras-061bdf.netlify.com/
 
-## Project setup
+The project uses `Node.js` and the `Create-React-App` starter. If you do not have Node >= 6.x installed, you can download it here: Node.js
+
+Once Node is installed, navigate to the directory where you want to store the app
 
 `git clone https://github.com/moumen-soliman/reactnd-project-myreads-starter.git`
 
@@ -14,52 +16,13 @@ The starter template contains all the CSS and HTML markup that may be used but o
 
 `npm i` OR `yarn`
 
+Once all of the dependencies have been installed you can launch the app with
+
 `npm run start` OR `yarn start`
+
+A new browser window should automatically open displaying the app. If it doesn't, navigate to http://localhost:3000/ in your browser
 
 ## Project pages
 
 - `/` Page and it's include all books shelf autmoatically added & rendered form API
 - `/search` Page and you can search on book and change it's shelf
-
-## Backend Server
-
-To simplify your development process, we've provided a backend server for you to develop against. The provided file [`BooksAPI.js`](src/BooksAPI.js) contains the methods you will need to perform necessary operations on the backend:
-
-- [`getAll`](#getall)
-- [`update`](#update)
-- [`search`](#search)
-
-### `getAll`
-
-Method Signature:
-
-```js
-getAll();
-```
-
-- Returns a Promise which resolves to a JSON object containing a collection of book objects.
-- This collection represents the books currently in the bookshelves in your app.
-
-### `update`
-
-Method Signature:
-
-```js
-update(book, shelf);
-```
-
-- book: `<Object>` containing at minimum an `id` attribute
-- shelf: `<String>` contains one of ["wantToRead", "currentlyReading", "read"]
-- Returns a Promise which resolves to a JSON object containing the response data of the POST request
-
-### `search`
-
-Method Signature:
-
-```js
-search(query);
-```
-
-- query: `<String>`
-- Returns a Promise which resolves to a JSON object containing a collection of a maximum of 20 book objects.
-- These books do not know which shelf they are on. They are raw results only. You'll need to make sure that books have the correct state while on the search page.
--- src/App.js
+++ src/App.js
@@ -1,87 +1,120 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-
-import Home from "./components/Home";
-import Search from "./components/Search";
+import React from 'react'
+import './App.css'
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 import * as BooksAPI from "./BooksAPI";
-import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-    BooksData: null,
-    BooksShelfs: [],
-    query: "",
-    searchList: null,
-    emptyList: false
-  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: []
+        }
+    }
 
-  changeShelf = (book, updatedShelf) => {
-    BooksAPI.update(book, updatedShelf).then(() => {
-      this.getBooks();
-    });
-  };
+    async componentDidMount() {
+        await this.getListOfBooks()
+    }
 
-  getBooks = () => {
-    BooksAPI.getAll().then(res => {
-      this.setState({ BooksData: res });
-      res.map(book => {
-        this.setState(prevState => ({
-          BooksShelfs: [...prevState.BooksShelfs, book.shelf]
-        }));
-      });
-    });
-  };
+    async getListOfBooks() {
+        const books = await BooksAPI.getAll();
+        this.setState({
+            books
+        })
+    }
 
-  searchQuery = query => {
-    BooksAPI.search(query.trim()).then(res =>
-      res && res.length
-        ? this.setState({ searchList: res, emptyList: false })
-        : this.setState({ emptyList: true })
-    );
-  };
+    async addBook(book) {
+        const newBook = await BooksAPI.get(book.id);
+        return newBook
+    }
 
-  componentDidMount() {
-    const {
-      state: { BooksShelfs }
-    } = this;
+    updateShelf = async (updatedBook, updatedList, pageName) => {
+        let {books} = this.state;
+        let isBookNotUpdated = true;
+        books = books.map((book) => {
+            if (book.id === updatedBook.id) {
+                Object.keys(updatedList).forEach((shelfName) => {
+                    if (updatedList[shelfName].includes(book.id)) {
+                        book.shelf = shelfName;
+                        isBookNotUpdated = false;
+                    }
+                });
+            }
+            return book;
+        });
 
-    this.getBooks();
-  }
+        if (isBookNotUpdated) {
+            if (pageName === "HOME") {
+                books = books.filter((item) => item.id !== updatedBook.id)
+            } else {
+                const newBook = await this.addBook(updatedBook)
+                books.push(newBook);
+            }
+        }
 
-  render() {
-    const {
-      state: { BooksData, BooksShelfs, searchList, emptyList }
-    } = this;
+        this.setState({
+            books,
+        });
+    };
 
-    return (
-      <div className="app">
-        <Switch>
-          <Route path="/search">
-            <Search
-              emptyList={emptyList}
-              searchList={searchList}
-              onSearch={this.searchQuery}
-              onShelfSelector={this.changeShelf}
-            />
-          </Route>
-          <Route exact path="/">
-            <Home
-              onShelfSelector={this.changeShelf}
-              BooksData={BooksData}
-              BooksShelfs={BooksShelfs}
-            />
-          </Route>
-        </Switch>
-      </div>
-    );
-  }
+    async changeBookShelf(e, book, pageName) {
+        const updatedList = await BooksAPI.update(book, e.target.value);
+        this.updateShelf(book, updatedList, pageName);
+    }
+
+    getBookSelectedValue(currentBook) {
+        const { books } = this.state;
+        let bookShelf = 'none';
+        books.forEach((book) => {
+            if (currentBook.id === book.id) {
+                bookShelf = book.shelf;
+            }
+        });
+        return bookShelf;
+    }
+
+    renderBook = (book, pageName = "HOME") => {
+        return (
+            <li key={book.id}>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url(${book.imageLinks ? book.imageLinks.smallThumbnail : ""})`
+                        }}></div>
+                        <div className="book-shelf-changer">
+                            <select onChange={(e) => this.changeBookShelf(e, book, pageName)} value={this.getBookSelectedValue(book)}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading" disabled={book.shelf === "currentlyReading"}>Currently Reading</option>
+                                <option value="wantToRead" disabled={book.shelf === "wantToRead"}>Want to Read</option>
+                                <option value="read" disabled={book.shelf === "read"}>Read</option>
+                                <option value="none" disabled={!book.shelf || book.shelf === ""}>None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors && book.authors.join(", ")}</div>
+                </div>
+            </li>
+        )
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/search">
+                        <Search renderBook={this.renderBook} />
+                    </Route>
+                    <Route path="/">
+                        <Home books={this.state.books} renderBook={this.renderBook} />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
 
-export default BooksApp;
+export default BooksApp
